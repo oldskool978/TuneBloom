ea3f4d7d3b29096798a0526a73203f8a945da082
David <48716362+oldskool978@users.noreply.github.com>
2026-08-26 03:03:37 -0400

    Delete themes directory

--- themes/easter_eggs/neon_pulse.js
+++
@@ -1,6 +0,0 @@
-export default function triggerNeonPulse(container) {
-  const pulse = document.createElement("div");
-  pulse.className = "easter-neon-pulse";
-  container.appendChild(pulse);
-  setTimeout(() => pulse.remove(), 1200);
-}
